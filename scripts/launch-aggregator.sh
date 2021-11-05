d94029bc82d1db2c6a555627a8d415e440fbd32b
Wouter Devriese <wouter.devriese@imec.be>
2021-11-05 11:16:27 +0100

    updated launch scripts

--- scripts/launch-aggregator.sh
+++ scripts/launch-aggregator.sh
@@ -4,4 +4,25 @@
 
 conda activate ibm-federated-learning 
 
-python -m ibmfl.aggregator.aggregator "examples/configs/PHR/script/config_aggregator.yml"
+# python -m ibmfl.aggregator.aggregator "examples/configs/PHR/script/config_aggregator.yml"
+
+# arguments to add: 
+# 1) scripts to execute: gender, sofa or fl
+
+case "$1" in 
+    "gender")
+        echo starting gender setup
+        python -m ibmfl.aggregator.aggregator "examples/configs/PHR/script/gender_config_aggregator.yml"
+        exit 1;;
+    "sofa")
+        echo starting sofa setup
+        python -m ibmfl.aggregator.aggregator "examples/configs/PHR/script/sofa_config_aggregator.yml"
+        exit 1;;
+    "fl")
+        echo starting federated learning setup
+        python -m ibmfl.aggregator.aggregator "examples/configs/PHR/script/fl_config_aggregator.yml"
+        exit 1;;
+    "") 
+        echo not enough arguments
+        exit 1;;
+esac
--- scripts/launch-hospitalN.sh
+++ scripts/launch-hospitalN.sh
@@ -4,4 +4,24 @@
 
 conda activate ibm-federated-learning 
 
-python -m ibmfl.party.party "examples/configs/PHR/script/config_hospital$1.yml"
+# arguments to add: 
+# 1) number of hospital
+# 2) scripts to execute: gender, sofa or fl
+
+case "$2" in 
+    "gender")
+        echo starting gender setup
+        python -m ibmfl.party.party "examples/configs/PHR/script/gender_config_hospital$1.yml"
+        exit 1;;
+    "sofa")
+        echo starting sofa setup
+        python -m ibmfl.party.party "examples/configs/PHR/script/sofa_config_hospital$1.yml"
+        exit 1;;
+    "fl")
+        echo starting federated learning setup
+        python -m ibmfl.party.party "examples/configs/PHR/script/fl_config_hospital$1.yml"
+        exit 1;;
+    "") 
+        echo not enough arguments
+        exit 1;;
+esac
