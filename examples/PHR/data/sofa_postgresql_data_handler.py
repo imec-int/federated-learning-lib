a76482c9ff7bbeb1ef5f7ecb1fb1ba02a83b6218
Vital D'haveloose <vital.dhaveloose@imec.be>
2021-11-15 17:22:17 +0100

    simplified resp scores, corrected cardiovasc. scores

--- examples/PHR/data/sofa_postgresql_data_handler.py
+++ examples/PHR/data/sofa_postgresql_data_handler.py
@@ -6,6 +6,9 @@ import logging
 import numpy as np
 import pandas as pd
 
+import multiprocessing as mp
+from multiprocessing.pool import ThreadPool
+
 from ibmfl.data.data_handler import DataHandler
 from ibmfl.util.datasets import load_mnist
 
@@ -13,7 +16,6 @@ logger = logging.getLogger(__name__)
 
 # TODO place at entry point of application
 load_dotenv()
-
 
 class PostgreSqlDataHandler(DataHandler):
     """
@@ -28,10 +30,10 @@ class PostgreSqlDataHandler(DataHandler):
         self.database = data_config['database']
         self.envPath = data_config['envPath']
 
-    def get_respiratory_data(self, cur):
+    def get_respiratory_scores(self, cur):
         '''
-        function querying the dB for respiratory entries (PaO2/FiO2).
-        Returns: array of patient-ids & SOFA scores based on PaO2/FiO2
+        Function calculating the respiratory components of SOFA scores.
+        Returns: array of tuples like (sourceDatabase, patientId, sofaScore)
         PaO2/FiO2 [mmHg (kPa)] 	SOFA score
         ≥ 400 (53.3) ->	0
         < 400 (53.3) ->	+1
@@ -41,76 +43,153 @@ class PostgreSqlDataHandler(DataHandler):
         #TODO is formula correct?
         #TODO do we need mechanically ventilated?
         '''
-        sql = """SELECT patientunitstayid,
-            pao2 / (fio2/100) as ademhaling
-            FROM eicu_crd.apacheapsvar;"""
-        cur.execute(sql)
-        data = [{'sourceDatabase': self.database, 'patientid': patient,  'ademhaling': ademhaling}
-                for (patient, ademhaling) in cur.fetchall()]
-        data = pd.DataFrame(data, columns=['patientid', 'ademhaling'])
-        data['ademhaling'].mask(
-            data['ademhaling'] == 100, None, inplace=True)  # probably empty values which were -1
-        data['ademhaling'].mask(
-            data['ademhaling'] < 100, 4, inplace=True)
-        data['ademhaling'].mask(
-            data['ademhaling'].between(100, 200), 3, inplace=True)
-        data['ademhaling'].mask(
-            data['ademhaling'].between(200, 300), 2, inplace=True)
-        data['ademhaling'].mask(
-            data['ademhaling'].between(300, 400), 1, inplace=True)
-        data['ademhaling'].mask(
-            data['ademhaling'] >= 400, 0, inplace=True)
-        print(data.head(10))
-        resp_data = data  # TODO calculate array of SOFA scores for respiratory entries
-        return resp_data
+        def to_sofa(ratio):
+            if ratio < 100: return 4
+            if ratio < 200: return 3
+            if ratio < 300: return 2
+            if ratio < 400: return 1
+            return 0
 
-    def get_nervous_system_data(self, cur):
-        '''
-        function querying the dB for nervous system entries (Glasgow Coma scale).
-        Returns: array of patient-ids & SOFA scores based on Glasgow Coma scale
-        '''
-        sql = """SELECT patientunitstayid,
-            eyes + verbal + motor as glasgow_coma_scale
-            FROM eicu_crd.apacheapsvar;
-            """
+        sql = """SELECT patientunitstayid, pao2 / (fio2/100) as ratio
+                 FROM eicu_crd.apacheapsvar
+                 WHERE pao2 > 0 AND fio2 > 0;"""
         cur.execute(sql)
-        data = [{'sourceDatabase': self.database, 'patientid': patient, 'gcs': gcs}
-                for (patient, gcs) in cur.fetchall()]
-        ns_data = data  # TODO calculate array of SOFA scores for ns entries
-        return ns_data
+        return [(self.database, patientId, to_sofa(ratio)) for (patientId, ratio) in cur.fetchall()]
 
-    def get_cardiovascular_system_data(self, cur):
+    def get_nervous_system_scores(self, cur):
         '''
-        function querying the dB for cardiovascular system entries 
-        Functioneren Hart/bloedsomloop: Gemiddelde bloeddruk (MAP) 
-        needs: MAP, dopamine, dobutamin, epinephrine, norepinephrine
-        MAP = (SBP + 2* DBP)/3 
-        ==> SBP = pasystolic
-        ==> DBP = padiastolic
-        TODO: dopamine:  
-        when lower(drugname) like '%(ml/hr)%' then round(cast(drugrate as numeric) / 3, 3) -- rate in ml/h * 1600 mcg/ml / 80 kg / 60 min, to convert in mcg/kg/min
-        when lower(drugname) like '%(mcg/kg/min)%' then cast(drugrate as numeric)
-
-        Returns: array of patient-ids & SOFA scores based on MAP and vasopressors
+        Function calculating the nervous system components of SOFA scores.
+        Returns: array of tuples like (sourceDatabase, patientId, sofaScore)
         '''
-        sql = """SELECT v.patientunitstayid,
-            (v.pasystolic + 2 * v.padiastolic) / 3 as MAP,
-            i.drugname,
-            i.drugrate
-            FROM eicu_crd.vitalperiodic v
-            FULL OUTER JOIN eicu_crd.infusiondrug i
-            ON v.patientunitstayid = i.patientunitstayid
-            WHERE lower(i.drugname) like '%dopamine%'
-            or lower(i.drugname) like '%dobutamin%'
-            or lower(i.drugname) like '%epinephrine%'
-            or lower(i.drugname) like '%norepinephrine%'
-            ORDER BY i.patientunitstayid;
-            """
+        sql = """SELECT patientunitstayid, eyes + verbal + motor as gcs
+            FROM eicu_crd.apacheapsvar
+            WHERE eyes > 0 AND verbal > 0 AND motor > 0;
+            """    
         cur.execute(sql)
-        data = [{'sourceDatabase': self.database, 'patientid': patient, 'map': map, 'drug': drugname, 'drugrate': drugrate}
-                for (patient, map, drugname, drugrate) in cur.fetchall()]
-        vs_data = data  # TODO calculate array of SOFA scores for ns entries
-        return vs_data
+        def to_sofa(gcs):
+            if gcs < 6: return 4
+            if gcs < 10: return 3
+            if gcs < 13: return 2
+            if gcs < 15: return 1
+            return 0
+        return [(self.database, patientId, to_sofa(gcs)) for (patientId, gcs) in cur.fetchall()]
+
+    def get_cardiovascular_system_scores(self, conn):
+        '''
+        Function calculating the cardiovascular system components of SOFA scores.
+        Returns: array of tuples like (sourceDatabase, patientId, sofaScore)
+        '''
+
+        pool = ThreadPool(mp.cpu_count())
+
+        mapSql = """SELECT patientunitstayid, MIN((systemicsystolic + 2 * systemicdiastolic)/3) AS map
+                    FROM eicu_crd.vitalperiodic
+                    WHERE systemicsystolic BETWEEN 50 AND 250
+                      AND systemicdiastolic BETWEEN 25 AND 225
+                    GROUP BY patientunitstayid
+                    ORDER BY patientunitstayid"""
+        mapResults = pool.apply_async(self.run_query, args=(conn, mapSql))
+
+        def drug_query(drugname:str, conv_factor:str, max_rate:str):
+            """Function to produce a drug query.
+            conv_factor is the factor to multiply the drugrate with to get to µg/kg/min.
+            max_rate is the outlier cutoff in the unit of the rows.
+            The queries return two columns: patientunitstayid and (converted) drugrate"""
+            return """SELECT patientunitstayid, MAX(drugrate::FLOAT * """+conv_factor+""") AS drugrate
+                      FROM eicu_crd.infusiondrug
+                      WHERE drugrate ~ E'^[\\\\d\\\\.]+$'
+                        AND drugname = '"""+drugname+"""'
+                        AND drugrate::FLOAT < """+max_rate+"""
+                      GROUP BY patientunitstayid
+                      ORDER BY patientunitstayid"""
+
+        dopamineResults = [
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dopamine (mcg/kg/min)', '1', '100'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dopamine (ml/hr)', '0.140', '500'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dopamine ()', '0.105', '500'))),
+        ]
+
+        dobutamineResults = [
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dobutamine (mcg/kg/min)', '1', '100'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dobutamine (ml/hr)', '0.066', '1000'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Dobutamine ()', '0.054', '1000'))),
+        ]
+        
+        epinephrineResults = [
+            pool.apply_async(self.run_query, args=(conn, drug_query('Epinephrine (mcg/kg/min)', '1', '10'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Epinephrine (ml/hr)', '0.00448', '500'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Epinephrine (mcg/min)', '0.03809', '75'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Epinephrine ()', '0.00291', '600'))),
+        ]
+        
+        norepinephrineResults = [
+            pool.apply_async(self.run_query, args=(conn, drug_query('Norepinephrine (mcg/kg/min)', '1', '2'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Norepinephrine (ml/hr)', '0.0037', '600'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Norepinephrine (mcg/min)', '0.018626', '150'))),
+            pool.apply_async(self.run_query, args=(conn, drug_query('Norepinephrine ()', '0.002218', '700'))),
+        ]
+
+        def merge_drug_results(results):
+            merged = []
+            for rows in [result.get() for result in results]:
+                for row in rows:
+                    (patId, rate) = row
+                    found = False
+                    for idx, (patId2, rate2) in enumerate(merged):
+                        if patId2 == patId:
+                            found = True
+                            if rate > rate2:
+                                merged[idx] = row
+                            break
+                    if not found:
+                        merged.append(row)
+            return merged
+
+        mapRows = mapResults.get()
+        dopamineRows = merge_drug_results(dopamineResults)
+        dobutamineRows = merge_drug_results(dobutamineResults)
+        epinephrineRows = merge_drug_results(epinephrineResults)
+        norepinephrineRows = merge_drug_results(norepinephrineResults)
+
+        pool.close()
+
+        def calculate_sofa_score(mapr, dopamineRate, dobutamineRate, epinephrineRate, norepinephrineRate):
+            if dopamineRate > 15 or epinephrineRate > 0.1 or norepinephrineRate > 0.1:
+                return 4
+            if dopamineRate > 5 or epinephrineRate > 0 or norepinephrineRate > 0:
+                return 3
+            if dopamineRate > 0 or dobutamineRate > 0:
+                return 2
+            if mapr < 70:
+                return 1
+            return 0
+
+        allPatientIds = [patId for (patId, _) in mapRows]
+        for drugRows in [dopamineRows, dobutamineRows, epinephrineRows, norepinephrineRows]:
+            for (patId, _) in drugRows:
+                if not patId in allPatientIds:
+                    allPatientIds.append(patId)
+
+        def get_value_or_0(rows, patientId):
+            for (patId, value) in rows:
+                if patId == patientId:
+                    return value
+            return 0
+
+        print("nb map's: " + str(len(mapRows)))
+        print("nb dopamines: " + str(len(dopamineRows)))
+        print("nb dobutamines: " + str(len(dobutamineRows)))
+        print("nb ephinephrines: " + str(len(epinephrineRows)))
+        print("nb norepinephrines: " + str(len(norepinephrineRows)))
+        print("nb patients: " + str(len(allPatientIds)))
+
+        return [(patId, calculate_sofa_score(
+                get_value_or_0(mapRows, patId),
+                get_value_or_0(dopamineRows, patId),
+                get_value_or_0(dobutamineRows, patId),
+                get_value_or_0(epinephrineRows, patId),
+                get_value_or_0(norepinephrineRows, patId),
+            )) for patId in allPatientIds]
 
     def get_liver_data(self, cur):
         '''
@@ -181,17 +260,17 @@ class PostgreSqlDataHandler(DataHandler):
 
         cur = conn.cursor()
         # TODO are we allowed (depends on dB) to query in parallel?
-        resp_data = self.get_respiratory_data(cur)
-        ns_data = self.get_nervous_system_data(cur)
-        cs_data = self.get_cardiovascular_system_data(cur)
-        liver_data = self.get_liver_data(cur)
-        coag_data = self.get_coagulation_data(cur)
-        kidney_data = self.get_kidneys_data(cur)
-        data = self.calc_sofa(resp_data, ns_data, cs_data,
-                              liver_data, coag_data, kidney_data)
+        resp_scores = self.get_respiratory_scores(cur)
+        ns_scores = self.get_nervous_system_scores(cur)
+        cs_scores = self.get_cardiovascular_system_scores(conn)
+        #liver_data = self.get_liver_data(cur)
+        #coag_data = self.get_coagulation_data(cur)
+        #kidney_data = self.get_kidneys_data(cur)
+        #data = self.calc_sofa(resp_data, ns_data, cs_data,
+        #                      liver_data, coag_data, kidney_data)
         conn.close()
 
-        return data, None
+        return cs_data, None
 
     def save_data(self, statement: str, rows):
         """Inserts the given rows (tuples) of data with the given INSERT statement.
@@ -214,3 +293,10 @@ class PostgreSqlDataHandler(DataHandler):
         return psycopg2.connect(host=self.host, port=self.port, database=self.database,
                                 user=dbUser, password=dbPw,
                                 sslmode='require')
+    
+    def run_query(self,conn,sql:str):
+        print("\nrunning query:        " + sql)
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            tuples = cur.fetchall()
+            return tuples
