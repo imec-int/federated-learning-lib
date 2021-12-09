1b2dc964de01cd23f357edbce7527ec1bcfc2cd3
Koen Ongena <koen.ongena@imec.be>
2021-12-09 12:04:19 +0100

    Adding the possibility to start the party with a specific env file as input

--- examples/PHR/party/main.py
+++ examples/PHR/party/main.py
@@ -1,78 +1,94 @@
+import getopt
+
 from dotenv import load_dotenv
 import os
 import re
 import sys
 
 from ibmfl.party.status_type import StatusType
-from ibmfl.party.party import Party
+from ibmfl.party.party import Party, logger
 
-load_dotenv()
 
-config_dict = {
-    "aggregator": {
-        "ip": os.getenv('AGGREGATOR_ADDR', '127.0.0.1'),
-        "port": os.getenv('AGGREGATOR_PORT', 5000),
-    },
-    "connection": {
-        "info": {
-            "ip": os.getenv('PARTY_ADDR', '0.0.0.0'),
-            "port": os.getenv('PARTY_PORT', 8085),
-            "tls_config": {
-                "enable": bool(os.getenv('PARTY_TLS_ENABLED', False)),
-            }
+def build_config():
+    return {
+        "aggregator": {
+            "ip": os.getenv('AGGREGATOR_ADDR', '127.0.0.1'),
+            "port": os.getenv('AGGREGATOR_PORT', 5000),
         },
-        "name": "FlaskConnection",
-        "path": "ibmfl.connection.flask_connection",
-        "sync": False,
-    },
-    "data": {
-        "info": {
-            "host": "edit-ph-eicu.postgres.database.azure.com",
-            "port": "5432",
-            "database": "hospital_" + os.environ['HOSP_ID'],
+        "connection": {
+            "info": {
+                "ip": os.getenv('PARTY_ADDR', '0.0.0.0'),
+                "port": os.getenv('PARTY_PORT', 8085),
+                "tls_config": {
+                    "enable": bool(os.getenv('PARTY_TLS_ENABLED', False)),
+                }
+            },
+            "name": "FlaskConnection",
+            "path": "ibmfl.connection.flask_connection",
+            "sync": False,
         },
-        "name": "PostgreSqlDataHandler",
-        "path": "examples.PHR.data."+os.environ['CASE']+"_postgresql_data_handler",
-    },
-    "local_training": {
-        "name": "LocalTrainingHandler",
-        "path": "examples.PHR.party.training.local_training_handler",
-    },
-    "model": {
-        "name": "NoopFLModel",
-        "path": "examples.PHR.model.noop_fl_model",
-        "spec": {
-            "model_name": "NoopFLModel",
+        "data": {
+            "info": {
+                "host": "edit-ph-eicu.postgres.database.azure.com",
+                "port": "5432",
+                "database": "hospital_" + os.environ['HOSP_ID'],
+            },
+            "name": "PostgreSqlDataHandler",
+            "path": "examples.PHR.data." + os.environ['CASE'] + "_postgresql_data_handler",
         },
-    },
-    "privacy": {
-        "metrics": True,
-    },
-    "protocol_handler": {
-        "name": "PartyProtocolHandler",
-        "path": "ibmfl.party.party_protocol_handler",
-    },
-}
+        "local_training": {
+            "name": "LocalTrainingHandler",
+            "path": "examples.PHR.party.training.local_training_handler",
+        },
+        "model": {
+            "name": "NoopFLModel",
+            "path": "examples.PHR.model.noop_fl_model",
+            "spec": {
+                "model_name": "NoopFLModel",
+            },
+        },
+        "privacy": {
+            "metrics": True,
+        },
+        "protocol_handler": {
+            "name": "PartyProtocolHandler",
+            "path": "ibmfl.party.party_protocol_handler",
+        },
+    }
+
 
 if __name__ == '__main__':
-    """Entry point for the application. Configuration is done using environment variables (.env file is automatically loaded):
+    """Entry point for the application. 
+    Configuration is done using environment variables (.env file is automatically loaded, unless the path to the env 
+    file is specified as a command line argument).:
+    
     - AGGREGATOR_ADDR: The address of the aggregator to register with, default 127.0.0.1 .
     - AGGREGATOR_PORT: The port to connect to the aggregator, default 5000 .
     - PARTY_ADDR:      The address to bind on/listen to, default 0.0.0.0 .
     - PARTY_PORT:      The port to bind on/listen to, default 8085 .
     - HOSP_ID:         The id of the hospital to simulate, required.
     - CASE:            The case to simulate, e.g. "sofa", required.
+    - DB_HOST          The host name of the database, defaults to localhost
+    - DB_PORT          The port of the database, defaults to 5432
     - DB_USER:         The username to connect to the database with, required.
     - DB_PASSWORD:     The password to connect to the database with, required.
+    - DB_DATABASE_NAME The name of the database, defaults to postgres
     """
 
-    p = Party(config_dict=config_dict)
+    args, opts = getopt.getopt(sys.argv[1:], "e:", ["env-file="])
+    env_path = '.env'
+    for opt, arg in args:
+        if opt in ("-e", "--env-file"):
+            logger.info("Loading environment from file: %s", arg)
+            if not os.path.exists(arg):
+                sys.exit("The path " + arg + "does not exists")
+            env_path = arg
 
+    load_dotenv(dotenv_path=env_path)
+
+    p = Party(config_dict=(build_config()))
     p.start()
-    p.proto_handler.connection.source_info['ip'] = os.getenv('PARTY_EXTERNAL_IP', os.getenv('PARTY_ADDR', '0.0.0.0'))
     p.register_party()
-    # Awful hack :( See https://github.com/IBM/federated-learning-lib/issues/53#issuecomment-886659388)
-    print("External IP is ", os.getenv('PARTY_EXTERNAL_IP', os.getenv('PARTY_ADDR', '0.0.0.0')))
 
     # Indefinite loop to accept user commands to execute
     while 1:
